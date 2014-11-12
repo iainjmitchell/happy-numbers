914ce5546dbe8798ac89bb7c3155d1093bfca8a0
iainjmitchell <iainjmitchell@gmail.com>
2014-11-12 13:00:10 +0000

    stuff

--- happyNumbers.js
+++ happyNumbers.js
@@ -1,14 +1,29 @@
 require('chai').should();
 
-describe('1', function(){
-	it('is a happy number', function(){
-		var mathematician = new Mathematician();
-		mathematician.isHappyNumber(1).should.be.true;
+var HAPPY_NUMBERS = [1];
+
+HAPPY_NUMBERS.forEach(function(number){
+	describe(number, function(){
+		it('is a happy number', function(){
+			var mathematician = new Mathematician();
+			mathematician.isHappyNumber(number).should.be.true;
+		});
+	});
+});
+
+var NON_HAPPY_NUMBERS = [2, 3];
+
+NON_HAPPY_NUMBERS.forEach(function(number){
+	describe(number, function(){
+		it('is not a happy number', function(){
+			var mathematician = new Mathematician();
+			mathematician.isHappyNumber(number).should.be.false;
+		});
 	});
 });
 
 var Mathematician = function(){
-	this.isHappyNumber = function(){
-		return true;
+	this.isHappyNumber = function(number){
+		return number === 1;
 	}
 }
